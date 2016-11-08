917341082e540d3e82848e8d4916e826d1e9ace1
Siva Surya <siv12345@users.noreply.github.com>
2016-11-08 14:31:09 +0530

    Update circ.java

--- Siva/circ.java
+++ Siva/circ.java
@@ -1,3 +1,9 @@
+// Write a program to fill in a two dimensional array in a circular fashion with natural numbers from 1 to n^2 given n as an input
+// if n = 4, then the array will be 
+// 1   2   3   4 
+// 12  13  14  5
+// 11  16  15  6
+// 10  9   8   7
 class circ
 {
    public void accept(int n)
